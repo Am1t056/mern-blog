bd9d30f1fd4096770b13c5a740fab12c6ce388a7
Amit Ghimire <amitghimireg@gmail.com>
2024-02-06 19:19:37 +0545

    update header component with user data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -85,7 +85,7 @@ export const google=async (req,res,next)=>{
                     profilePicture:googlePhotoUrl,
                 });
                 await newUser.save();
-                const token=jwt.sign({id:newUser,_id},process.env.JWT_SECRET);
+                const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET);
                 const {password,...rest}=newUser._doc;
                 res.status(200).cookie("access_token",token,{
                     httpOnly:true,
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link,useLocation } from 'react-router-dom'
+import { Avatar, Button, Dropdown, DropdownDivider, Navbar, TextInput } from 'flowbite-react';
+import React from 'react';
+import { Link,useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa'
+import {FaMoon} from 'react-icons/fa';
+import {useSelector} from 'react-redux';
 
 function Header() {
     const path=useLocation().pathname;
+    const {currentUser}=useSelector(state=>state.user);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className=' self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -21,9 +23,25 @@ function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon/>
             </Button>
-            <Link to='/sign-in'>
+            {currentUser ?(
+                <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded></Avatar>}>
+                   <Dropdown.Header><span className='block'>@{currentUser.username}</span></Dropdown.Header> 
+                  <Link to={'/dashboard?tab=profile'}>
+                    <Dropdown.Item>profile</Dropdown.Item>
+                  </Link>
+                  <DropdownDivider className=' bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500'/>
+                  <Dropdown.Item>Sign Out</Dropdown.Item>
+                  <DropdownDivider className=' bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500'/>
+
+                </Dropdown>
+            ):
+             <Link to='/sign-in'>
                 <Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
             </Link>
+            
+         
+        }
+           
             <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
